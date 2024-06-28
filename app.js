3bc6b97ad7c89dec22c6621989acc52e7bf7ba4e
thonino <thoninoben@gmail.com>
2024-06-28 23:30:47 +0200

    socket.io 50%

--- app.js
+++ app.js
@@ -1,53 +1,60 @@
-//  - - - - - - - - -D E P E N D A N C E S- - - - - - - - - - //
-
 // express & express-session
 const express = require('express');
 const session = require('express-session');
 const app = express();
+const http = require('http');
+const socketIo = require('socket.io');
+const sharedSession = require('express-socket.io-session');
+const server = http.createServer(app);
+const io = socketIo(server);
 
 // bcrypt
 const bcrypt = require('bcrypt');
 
 // MongoDB Mongoose et dotenv
 require('dotenv').config();
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const Message = require('./models/Message');
 const User = require('./models/User');
 const { log } = require('console');
 const url = process.env.DATABASE_URL;
-mongoose.connect(url, {useNewUrlParser: true,useUnifiedTopology: true})
-.then(() => {console.log("MongoDB connected");})
-.catch(err => {console.log(err);});
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => { console.log("MongoDB connected"); })
+  .catch(err => { console.log(err); });
 
-// EJS : 
+// EJS :
 app.set('view engine', 'ejs');
 
 // BodyParser
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({
-secret: 'your-secret-key',
-resave: false,
-saveUninitialized: false
-}));
+
+const sessionMiddleware = session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false
+});
+app.use(sessionMiddleware);
 
 // Method-override :
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 
 // get datetime
-// npm install moment --save
-const moment = require('moment'); 
+const moment = require('moment');
 
 app.use('/public', express.static('public'));
 
-//  - - - - - - - - - - R O U T E - - - - - - - - - - - //
+// Partager la session entre Express et Socket.IO
+io.use(sharedSession(sessionMiddleware, {
+  autoSave: true
+}));
 
 // GET HOME
 app.get('/', (req, res) => {
   const user = req.session.user;
   const heure = moment().format('DD-MM-YYYY, h:mm:ss');
-  res.render('home', { user: user, heure: heure} ); 
+  res.render('home', { user: user, heure: heure });
 });
 
 // GET REGISTER
@@ -57,102 +64,102 @@ app.get('/register', (req, res) => {
 });
 
 // POST REGISTER
-app.post('/register', function(req, res){
-const userData = new User({
-  pseudo: req.body.pseudo,
-  email: req.body.email,
-  password: bcrypt.hashSync(req.body.password,10),
-  role: req.body.role
-  })
-  userData.save()
-    .then(()=>{ res.redirect('/login')})
-    .catch((err)=>{console.log(err); 
+app.post('/register', function (req, res) {
+  const userData = new User({
+    pseudo: req.body.pseudo,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 10),
+    role: req.body.role
   });
+  userData.save()
+    .then(() => { res.redirect('/login') })
+    .catch((err) => { console.log(err); });
 });
 
 // GET LOGIN
-app.get('/login', (req, res) => {const user = req.session.user;
-res.render('LoginForm', { user: user });});
+app.get('/login', (req, res) => {
+  const user = req.session.user;
+  res.render('LoginForm', { user: user });
+});
 
 // POST LOGIN
 app.post('/login', (req, res) => {
-User.findOne({ pseudo: req.body.pseudo }).then(user => {
-  if (!user) {res.send('Pseudo invalide');}
-  if (!bcrypt.compareSync(req.body.password, user.password)) {
-    res.send('Mot de passe invalide');}
-  req.session.user = user;
-  res.redirect('/userpage');
-})
-.catch(err => console.log(err));
+  User.findOne({ pseudo: req.body.pseudo }).then(user => {
+    if (!user) { res.send('Pseudo invalide'); }
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      res.send('Mot de passe invalide');
+    }
+    req.session.user = user;
+    res.redirect('/userpage');
+  })
+    .catch(err => console.log(err));
 });
 
 // GET LOGOUT
 app.get('/logout', (req, res) => {
-req.session.destroy((err) => {
-  if (err) {console.log(err);} 
-  else {res.redirect('/login');}
-});
+  req.session.destroy((err) => {
+    if (err) { console.log(err); }
+    else { res.redirect('/login'); }
+  });
 });
 
 // GET USER PAGE
-
 app.get('/userpage', (req, res) => {
-  
-  if (!req.session.user) { return res.redirect('/login');}
+  if (!req.session.user) { return res.redirect('/login'); }
+
   const user = req.session.user;
-  Message.find({$or: [{ expediteur: user.pseudo }, { destinataire: user.pseudo }]
+  Message.find({
+    $or: [{ expediteur: user.pseudo }, { destinataire: user.pseudo }]
   }).then(messages => {
     const messagesSent = messages.filter(message => message.expediteur === user.pseudo);
     const messagesReceived = messages.filter(message => message.destinataire === user.pseudo);
-    res.render('userpage', {  
+    res.render('userpage', {
       user: user, messagesSent: messagesSent,
-      messagesReceived: messagesReceived });
+      messagesReceived: messagesReceived
+    });
   })
-  .catch(err => {console.log(err);});
+    .catch(err => { console.log(err); });
 });
 
-app.get('/userpage/:pseudo', (req, res) => {
-  
-  if (!req.session.user) { return res.redirect('/login');}
+// GET DIALOGUE
+app.get('/dialogue/:pseudo', (req, res) => {
+  if (!req.session.user) { return res.redirect('/login'); }
   const user = req.session.user;
-  var pseudo = req.params.pseudo;
-  Message.find({$or: [{ expediteur: user.pseudo }, { destinataire: user.pseudo }]
+  const pseudo = req.params.pseudo;
+  Message.find({
+    $or: [{ expediteur: user.pseudo }, { destinataire: user.pseudo }]
   }).then(messages => {
-    // const heure = moment().format('DD-MM-YYYY, h:mm:ss');
     const heure = moment().format('h:mm:ss');
-    // filtrer messages pour afficher le dialogue 
     const messagesFilter = messages.filter(
-      message =>  (message.expediteur === user.pseudo && message.destinataire === pseudo) ||
-                  (message.destinataire === user.pseudo && message.expediteur === pseudo)
+      message => (message.expediteur === user.pseudo && message.destinataire === pseudo) ||
+        (message.destinataire === user.pseudo && message.expediteur === pseudo)
     );
-    const ContactsMsg = messages.filter
-      (message => (message.destinataire === user.pseudo) ||
-                  (message.expediteur === user.pseudo));
-    res.render('Dialogue', {  
+    const ContactsMsg = messages.filter(
+      message => (message.destinataire === user.pseudo) ||
+        (message.expediteur === user.pseudo));
+    res.render('Dialogue', {
       heure: heure,
       user: user,
-      pseudo: pseudo, 
+      pseudo: pseudo,
       messagesFilter: messagesFilter,
-
-      ContactsMsg: ContactsMsg, });
+      ContactsMsg: ContactsMsg,
+    });
   })
-  .catch(err => {console.log(err);});
+    .catch(err => { console.log(err); });
 });
 
 // GET NEW MESSAGE
 app.get('/message/new', (req, res) => {
-if (!req.session.user) {return res.redirect('/login');}
-const user = req.session.user;
-// const heure = moment().format('DD-MM-YYYY, h:mm:ss');
-const heure = moment().format('h:mm:ss');
-res.render('messageForm', { user: user, heure: heure});
+  if (!req.session.user) { return res.redirect('/login'); }
+  const user = req.session.user;
+  const heure = moment().format('h:mm:ss');
+  res.render('messageForm', { user: user, heure: heure });
 });
 
 // POST NEW MESSAGE
 app.post('/message', (req, res) => {
-  if (!req.session.user) {return res.redirect('/login');}
+  if (!req.session.user) { return res.redirect('/login'); }
   const user = req.session.user;
-  // const heure = moment().format('DD-MM-YYYY, h:mm:ss');
   const heure = moment().format('h:mm:ss');
   const messageData = new Message({
     expediteur: user.pseudo,
@@ -161,50 +168,45 @@ app.post('/message', (req, res) => {
     datetime: heure
   });
   messageData.save()
-    .then(() => res.redirect(`/userpage/${req.body.destinataire}`))
+    .then(() => res.redirect(`/dialogue/${req.body.destinataire}`))
     .catch(err => {
       console.log(err);
     });
 });
 
-// GET EDIT PAGE 
+// GET EDIT PAGE
 app.get('/edit-message/:id', (req, res) => {
-const user = req.session.user;
-// const heure = moment().format('DD-MM-YYYY, h:mm:ss');
-const heure = moment().format('h:mm:ss');
-Message.findById(req.params.id)
-  .then((message) => {res.render('edit', { 
-    message: message , user: user, heure: heure });})
-  .catch(err => {console.log(err);});
+  const user = req.session.user;
+  const heure = moment().format('h:mm:ss');
+  Message.findById(req.params.id)
+    .then((message) => {
+      res.render('edit', {
+        message: message, user: user, heure: heure
+      });
+    })
+    .catch(err => { console.log(err); });
 });
 
 // PUT EDIT PAGE
 app.put('/edit-message/:id', (req, res) => {
-// const heure = moment().format('DD-MM-YYYY, h:mm:ss');
-const heure = moment().format('h:mm:ss');
-const messageData = {
-  destinataire: req.body.destinataire,
-  message: req.body.message,
-  datetime: heure
-};
-Message.findByIdAndUpdate(req.params.id, messageData)
-  .then(() => {res.redirect(`/userpage/${req.body.destinataire}`);})
-  .catch(err => {console.log(err);});
+  const heure = moment().format('h:mm:ss');
+  const messageData = {
+    destinataire: req.body.destinataire,
+    message: req.body.message,
+    datetime: heure
+  };
+  Message.findByIdAndUpdate(req.params.id, messageData)
+    .then(() => { res.redirect(`/dialogue/${req.body.destinataire}`); })
+    .catch(err => { console.log(err); });
 });
 
-// DELETE 
-// app.delete('/delete-message/:messageId', (req, res) => {
-// Message.findByIdAndRemove(req.params.messageId)
-//   .then(() => {res.redirect(`/userpage/${pseudo}`);})
-//   .catch(err => {console.log(err);});
-// });
-
+// DELETE MESSAGE
 app.delete('/delete-message/:messageId', (req, res) => {
   const messageId = req.params.messageId;
-  const pseudo = req.body.pseudo; 
+  const pseudo = req.body.pseudo;
   Message.findByIdAndRemove(messageId)
     .then(() => {
-      res.redirect(`/userpage/${pseudo}`);
+      res.redirect(`/dialogue/${pseudo}`);
     })
     .catch(err => {
       console.log(err);
@@ -212,9 +214,36 @@ app.delete('/delete-message/:messageId', (req, res) => {
     });
 });
 
+// Socket.IO: Écouter les connexions des clients
+io.on('connection', (socket) => {
+  const user = socket.handshake.session.user;
+  if (user) {
+    console.log(user.pseudo + ' est connecté');
 
+    // Écouter les messages du client
+    socket.on('sendText', (text) => {
+      const heure = moment().format('h:mm:ss');
+      const newMessage = new Message({
+        expediteur: user.pseudo,
+        destinataire: socket.handshake.query.destinataire,
+        message: text,
+        datetime: heure
+      });
 
-// Démarrage du serveur
-var server = express(); app.listen(5001, function () {
-console.log("server listening : http://localhost:5001");
+      newMessage.save()
+        .then(() => {
+          io.emit('receiveText', { pseudo: user.pseudo, text: text, datetime: heure });
+        })
+        .catch(err => console.log(err));
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Un utilisateur s\'est déconnecté');
+    });
+  }
+});
+
+const PORT = 5001;
+server.listen(PORT, () => {
+  console.log(`Serveur écoutant sur le port ${PORT}`);
 });
--- public/js/dialogue.js
+++ public/js/dialogue.js
@@ -15,4 +15,36 @@ document.querySelector("#toggle").addEventListener("click", () => {
   });
 });
 
+// socket.io
+// Initialisation de la connexion Socket.IO avec le destinataire comme query parameter
+const socket = io({
+  query: {
+    destinataire: document.querySelector('input[name="destinataire"]').value
+  }
+});
+const textInput = document.getElementById('textInput');
+const sendButton = document.getElementById('sendButton');
+const messageBox = document.getElementById('messageBox');
+
+sendButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const text = textInput.value;
+  socket.emit('sendText', text);
+  textInput.value = '';
+});
+
+socket.on('receiveText', (data) => {
+  const messageDiv = document.createElement('div');
+  messageDiv.classList.add('d-flex', 'justify-content-end', 'fs-5', 'me-3', 'bg-light', 'pe-2', 'pt-2', 'rounded');
+  messageDiv.innerHTML = `
+    <p class="text-capitalize fst-italic fw-bold text-info">${data.pseudo} :</p>
+    <p class="fw-light text-dark fst-italic ms-2">${data.text}</p>
+  `;
+  messageBox.prepend(messageDiv);
+});
+
+
+
+
+
 
