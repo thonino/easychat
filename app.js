4b6e659f0ab096560d3a6f3517b699cb08214f8d
thonino <thoninoben@gmail.com>
2024-08-20 16:31:43 +0200

    page SendMail added, update design

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 
 const nodemailer = require('nodemailer');
-// Configurer le transporteur SMTP réutilisable pour l'envoi d'e-mails
+// Configurer le transporteur SMTP 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -26,16 +26,11 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-
 // Vérifier la configuration du transporteur
 transporter.verify(function(error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take our messages');
-  }
+  if (error) { console.log(error); } 
+  else{ console.log('Mailer configuration succefull');}
 });
-
 
 // Importation des modèles
 const Message = require('./models/Message');
@@ -284,6 +279,25 @@ app.get('/image/:filename', async (req, res) => {
   }
 });
 
+// Route Test pour avoyer des mail
+// app.get('/sendmail', (req, res) => {
+//   res.render('SendMail')
+// })
+// app.post('/sendmail', (req, res) => {
+//   const { email, text, subject } = req.body;
+//   const mailOptions = {
+//     from: 'easychat.freeapp@gmail.com',
+//     to: email,
+//     subject: subject,
+//     text: text
+//   };
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) { console.error(error);
+//       return res.status(500).send("<h1>Error sending email</h1>");
+//     }
+//     res.send(`<h1>Un mail a été envoyé sur ${email}</h1>`);
+//   });
+// });
 
 // Route pour afficher la page de compte utilisateur
 app.get('/account/:pseudo', (req, res) => {
@@ -295,7 +309,6 @@ app.get('/account/:pseudo', (req, res) => {
   const logoPhoto = res.locals.logoPhoto;
   res.render('Account', { user, logoPhoto });
 });
-
 
 // Index
 app.get('/', (req, res) => {
@@ -410,7 +423,7 @@ app.post('/passwordforgot', async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(404).send("<h1>Email address not found</h1>"); // email
+      return res.status(404).send("<h1>Email introuvable</h1>"); 
     }
     const token = crypto.randomBytes(20).toString('hex');
     user.token = token;
@@ -428,11 +441,11 @@ app.post('/passwordforgot', async (req, res) => {
         console.error(error);
         return res.status(500).send("<h1>Error sending email</h1>");
       }
-      res.send(`<h1>Please check your email box: ${email}</h1>`);
+      res.send(`<h1>Un mail vous a été envoyé sur ${email}</h1>`);
     });
   } catch (error) {
     console.error(error);
-    res.status(500).send("<h1>Reset request error</h1>");
+    res.status(500).send("<h1>Echec de la Requête</h1>");
   }
 });
 
@@ -449,17 +462,17 @@ app.post('/passwordreset', async (req, res) => {
       tokenExpires: { $gt: Date.now() }
     });
     if (!user) {
-      return res.status(400).send("<h1>Invalid or expired token</h1>");
+      return res.status(400).send("<h1>Erreur ou token expiré</h1>");
     }
     user.password = bcrypt.hashSync(password, 10);
     user.token = undefined;
     user.tokenExpires = undefined;
     await user.save();
     
-    res.send("<h1>Successfully reset password</h1>");
+    res.send("<h1>Réinitialisation du mot de passe réussi</h1>");
   } catch (error) {
     console.error(error);
-    res.status(500).send("<h1>Reset password error</h1>");
+    res.status(500).send("<h1>Echec de la réinitialisation</h1>");
   }
 });
 
